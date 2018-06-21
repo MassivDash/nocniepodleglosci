2e17fe77e0a44af9b6090289ff405906c7291a4b
MassivDash <lukasz.celitan@gmail.com>
2018-06-21 23:10:26 +0200

    added facebook icon to the intro page

--- src/pages/Sekcje/start.js
+++ src/pages/Sekcje/start.js
@@ -1,7 +1,9 @@
 import React from 'react'; 
 import Noc from '../../img/Noc.svg'
 import Numbers from '../../img/numbers.svg'
+import facebook from '../../img/fb.svg'
 import './start.sass'
+
 
 const Start = (props) => {
     
@@ -15,7 +17,8 @@ const Start = (props) => {
     <section className={style} id="start">
     <div className="hero-body">
         <div className="container has-text-centered intro">
-
+            
+            <div className="fbicon"><a href="https://facebook.com/nocniepodleglosci/" target="_blanc"><img src={facebook} alt="noc niepodległości"/>/nocniepodleglosci/</a></div>
             <div className="Noc">
                 <img className="Noclogo" src={Noc} alt="Noc Niepodległsości"/><img className="NocNum" src={Numbers} alt="Noc Niepodległości"/>
                 <div className="NocTitle">
