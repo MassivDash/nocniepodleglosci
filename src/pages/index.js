434895d8bc47a7510bcb1d99cce5cf9209b68faa
MassivDash <lukasz.celitan@gmail.com>
2018-06-21 13:20:59 +0200

    reverted to older version (unknown bug on observer API)

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,15 +42,11 @@ setStartVisible4 = ({ isIntersecting }) => {
             require('smooth-scroll')('a[href*="#"]');
         }
 
-       
-
         console.log(this.state);
 
         const {data} = this.props
         const {edges: posts} = data.allMarkdownRemark
-        const options = {
-          threshold: 0.5
-        }
+
         console.log(this.state);
         return (
 
@@ -85,18 +81,15 @@ setStartVisible4 = ({ isIntersecting }) => {
 
                 <div className="separtor asfaltbackground"></div>
 
-                <Observer 
-                onChange={this.setStartVisible2} 
-                >
-   >
+                <Observer onChange={this.setStartVisible2} >
                   <div>
                     <Fun isVisible={this.state.inView2}/>
                     </div>
                 </Observer>
 
-                <Observer  onChange={this.setStartVisible3} >
+                <Observer onChange={this.setStartVisible3} >
                   <div>
-                    <Imprezy  isVisible={this.state.inView3} posts={posts}/>
+                    <Imprezy isVisible={this.state.inView3} posts={posts}/>
                     </div>
                 </Observer>
 
