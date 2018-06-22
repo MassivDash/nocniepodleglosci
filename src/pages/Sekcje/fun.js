4d18334dbdeeb8b94330a745fe27778ee194bb75
MassivDash <lukasz.celitan@gmail.com>
2018-06-22 11:46:40 +0200

    imported the new font with polish symbols

--- src/pages/Sekcje/fun.js
+++ src/pages/Sekcje/fun.js
@@ -13,27 +13,12 @@ const Fun = (props) => {
             <div className="container">
 
                 <div className="columns">
-                    <div className="column is-one-third">
-                        <div className="content">
-                            <span className="image-wrap">
-                            <img className="pilusdckiImage" src={pil} alt="non niepodleglosci"/>
-                            </span>
-                        </div>
-                    </div>
-                    <div className="column">
+                        <div className="column">
                         <div className="fun content">
                             <p>
-                                Pragniemy, aby w tę noc, podobnie jak w Sylwestra, gdy zaczynamy świętować Nowy
-                                Rok, rozpocząć obchodzenie Święta Niepodległości. Chcemy skorzystać z tego, że
-                                możemy trochę mocniej zaimprezować, gdyż 11 listopada jest dniem ustawowo wolnym
-                                od pracy.
+                                Zapraszamy do wspólnej zabawy z okazji <span><br></br>Święta Niepodległości !</span>
                             </p>
-                            <p>Nasze święto jest totalnie wyluzowane i dalekie od oficjalnej spiny. Chcemy
-                                odwoływać się do zupełnie naturalnego poczucia dumy i chęci okazania swojego
-                                przywiązania do naszego kraju. <u>Przy czym warto podkreślać, iż można to czynić w
-                                sposób radosny, otwarty, wolny od agresji, bazujący na naszych wspaniałych
-                                cechach - poczuciu humoru i słowiańskiej skłonności do uciech i zabaw.</u>
-                            </p>
+
                         </div>
                     </div>
                 </div>
@@ -41,14 +26,14 @@ const Fun = (props) => {
                     <div className="column">
                         <div className="content fungrid">
 
-                            <p className="a padding" >
-                                Noc Niepodległości z 10 na 11 listopada ma być ogólnopolską, powszechną imprezą
-                                z okazji Święta Niepodległości.
+                            <p className="a padding">
+                                <u>Noc Niepodległości z 10 na 11 listopada</u>
+                                ma być ogólnopolską, powszechną imprezą z okazji Święta Niepodległości.
                             </p>
                             <p className="b padding">
-                            Chcemy pokazać, że ludzie pogodni, wyluzowani mogą być najzwyczajniej w świecie
-                            dumni z tego, że pochodzą z Kraju nad Wisłą, a imprezowanie może być jedną z
-                            narodowych cnót sprzyjających integracji i budowaniu wspólnoty.
+                                Chcemy pokazać, że ludzie pogodni, wyluzowani mogą być najzwyczajniej w świecie
+                                dumni z tego, że pochodzą z Kraju nad Wisłą, a imprezowanie może być jedną z
+                                narodowych cnót sprzyjających integracji i budowaniu wspólnoty.
                             </p>
                             <p className="c padding">
                                 Pragniemy aby Święto Niepodległości kojarzyło się z dumą, aby patriotyzm nabrał
@@ -56,19 +41,15 @@ const Fun = (props) => {
                                 nowoczesnej Ojczyźnie.
                             </p>
                             <p className="d padding">
-                            Skłonność do zabaw jest jedną z naszych najfajniejszych cech narodowych, którą
-                            warto celebrować, pielęgnować i szukać sposobności dla jej utrwalenia.
+                                Skłonność do zabaw jest jedną z naszych najfajniejszych cech narodowych, którą
+                                warto celebrować, pielęgnować i szukać sposobności dla jej utrwalenia.
                             </p>
                             <p className="e padding">
-                            Jednym z najpopularniejszych i najbardziej naturalnych sposobów świętowania jest
-                            zabawa, a jak się bawić to nie indywidualnie tylko, parafrazując słowa piosenki
-                            Jeremiego Przybory, wespół w zespół! Dlatego zachęcamy polskie zespoły i
-                            wykonawców, żeby w tę noc rozkręcali ogólnopolską imprezę. W klubach,
-                            restauracjach, mieszkaniach zorganizujmy imprezy, spotkania, domówki z okazji
-                            Nocy Niepodległości. Do akcji wciągamy kina, opery, filharmonie (polski
-                            repertuar) muzea i inne ośrodki kulturalne. Wspólnym elementem ubioru
-                            uczestników niech będzie biało-czerwony detal; wstążka, kotylion, itp. W trakcie
-                            imprez podstawę diety stanowi patriotka – biało-czerwone ciastko i pijemy biało-czerwone napoje.
+                                Nasze święto jest totalnie wyluzowane i dalekie od oficjalnej spiny. Chcemy
+                                odwoływać się do zupełnie naturalnego poczucia dumy i chęci okazania swojego
+                                przywiązania do naszego kraju. Przy czym warto podkreślać, iż można to czynić w
+                                sposób radosny, otwarty, wolny od agresji, bazujący na naszych wspaniałych
+                                cechach; poczuciu humoru i słowiańskiej skłonności do uciech i zabaw.
                             </p>
                             <p className="f padding">
                                 Jesteśmy głęboko przekonani, że Noc Niepodległości stanie się jednym z
