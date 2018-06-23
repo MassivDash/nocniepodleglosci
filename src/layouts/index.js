51470becbea4475b675b80a4250bb66afb2dbb01
MassivDash <lukasz.celitan@gmail.com>
2018-06-23 12:37:34 +0200

    seperating the font face from all scss

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Footer from '../components/Footer/Footer'
-
-
-import Navbar from '../components/Navbar/Navbar'
 import './all.sass'
+import './font-face.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
