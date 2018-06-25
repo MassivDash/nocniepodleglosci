bea97777574f7936b773cf8bdc47dabb68114aae
MassivDash <lukasz.celitan@gmail.com>
2018-06-25 17:05:02 +0200

    fixing slider for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,8 +66,8 @@ setStartVisible4 = ({ isIntersecting }) => {
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 1,
+            slidesToScroll: 1,
             infinite: true,
             
           }
@@ -75,19 +75,19 @@ setStartVisible4 = ({ isIntersecting }) => {
         {
           breakpoint: 600,
           settings: {
-            centerMode: true,
+            
             slidesToShow: 1,
             slidesToScroll: 1,
-            initialSlide: 0
+            
           }
         },
         {
           breakpoint: 480,
           settings: {
-            centerMode: true,
+            
             slidesToShow: 1,
             slidesToScroll: 1,
-            initialSlide: 0
+            
           }
         }
       ]
