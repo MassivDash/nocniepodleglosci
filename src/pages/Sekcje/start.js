4201176b610c09d787a01a5a690990e66b85d39a
SpaceGhost <lukasz.celitan@gmail.com>
2018-07-20 19:54:54 +0200

    moon width change for mobile

--- src/pages/Sekcje/start.js
+++ src/pages/Sekcje/start.js
@@ -12,8 +12,8 @@ const Start = (props) => {
 
     return (
         <section className={style} id="start">
+        <div className="cloudsfront"></div>
             <div className="hero-body">
-                <div className="cloudsfront"></div>
                 <div className="container has-text-centered intro">
 
                     <div className="fbicon">
